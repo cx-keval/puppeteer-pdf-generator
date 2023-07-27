e90ea35b0ced379ec10af4bdafba85cce344f703
Keval <kevalpm@gmail.com>
2023-07-27 11:20:51 +0530

    Project Initialized and basic setup done

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import dotenv from "dotenv"
+import express from "express"
+import morgan from "morgan"
+import cors from "cors"
+import helmet from "helmet"
+import router from "./routes"
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 5005
+
+//cors & helmet => for security
+app.use(cors())
+app.use(helmet())
+
+// used to log requests
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"))
+}
+
+app.use(express.json())
+
+app.use("/api/pdf/", router)
+
+app.listen(PORT, () =>
+    console.log("Server is running at http://localhost:" + PORT)
+)
