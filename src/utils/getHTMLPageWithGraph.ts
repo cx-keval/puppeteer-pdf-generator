3c4312fec3fdd76130ea8cf9225de46e44a9822c
Keval <kevalpm@gmail.com>
2023-07-28 17:38:27 +0530

    vanilla template for GS:IG profile done and added

--- src/utils/getHTMLPageWithGraph.ts
+++ src/utils/getHTMLPageWithGraph.ts
@@ -16,6 +16,26 @@ export const getHTMLPageWithGraph = (
     <body>
         ${children}
         <script>
+        const SI_SYMBOL = ["", "k", "M", "G", "T", "P", "E"];
+
+            const millify = (number) => {
+                // what tier? (determines SI symbol)
+                let tier = Math.log10(Math.abs(number)) / 3 | 0;
+
+                // if zero, we don't need a suffix
+                if (tier == 0) return number;
+
+                // get suffix and determine scale
+                let suffix = SI_SYMBOL[tier];
+                let scale = Math.pow(10, tier * 3);
+
+                // scale the number
+                let scaled = number / scale;
+
+                // format number and add suffix
+                return scaled.toFixed(1) + suffix;
+            }
+
             const data = JSON.parse('${stringifiedData}')
 
             const series = [
@@ -59,7 +79,7 @@ export const getHTMLPageWithGraph = (
                 yaxis: {
                     labels: {
                         formatter: (value) => {
-                            return value
+                            return millify(+value)
                         }
                     }
                 },
